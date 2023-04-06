4320dd092519f36d3bd48d4e2943459dafefe2b8
karim132 <karim5@gmail.com>
2023-04-06 15:10:12 +0200

    add all

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,11 +4,26 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Project;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+
+        for($i=0; $i<10; $i++) {
+            $projet = new Project();
+            $projet ->setTitle('Les projets de prepavenir numero '.$i)
+                   ->setDescription('lorem ipsum'.$i)
+                   ->setImage('ipsum lorem'.$i);
+                   
+                   
+    
+                //Ajouter l'info à la base de données
+            $manager->persist($projet);
+            
+           
+        }
         // $product = new Product();
         // $manager->persist($product);
 
